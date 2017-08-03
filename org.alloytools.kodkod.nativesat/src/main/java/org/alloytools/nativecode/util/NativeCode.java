974d48df436950297bdd43cbf2f4ae61115c017e
Peter Kriens <peter.kriens@aqute.biz>
2017-08-03 14:58:22 +0200

    [native] Match properties

--- org.alloytools.kodkod.nativesat/src/main/java/org/alloytools/nativecode/util/NativeCode.java
+++ org.alloytools.kodkod.nativesat/src/main/java/org/alloytools/nativecode/util/NativeCode.java
@@ -30,14 +30,15 @@ public class NativeCode {
 	}
 	static Map<String,Path> cached = new ConcurrentHashMap<>();
 
-	public static Platform		AMD64_LINUX		= new Platform("linux", "amd64", "amd64-linux");
-	public static Platform		x86_64_LINUX	= new Platform("linux", "x86-64", "linux_x86_64");
-	public static Platform		X86_FREEBSD		= new Platform("freebsd", "x86.*", "x86-freebsd");
-	public static Platform		X86_LINUX		= new Platform("linux", "x86.*", "x86-linux");
+//	public static Platform		AMD64_LINUX		= new Platform("linux", "amd64", "amd64-linux");
+//	public static Platform		x86_64_LINUX	= new Platform("linux", "x86-64", "linux_x86_64");
+//	public static Platform		X86_FREEBSD		= new Platform("freebsd", "x86.*", "x86-freebsd");
+	public static Platform		X86_LINUX		= new Platform("linux", ".*86.*", "x86-linux");
 	public static Platform		X86_MAC			= new Platform("mac\\s*os.*", "ppc|power|powerpc.*|x86.*", "x86-mac");
 	public static Platform		X86_WINDOWS		= new Platform("win.*", "x86.*", "x86-windows");
 	public static Platform[]	platforms		= {
-			AMD64_LINUX, x86_64_LINUX, X86_FREEBSD, X86_LINUX, X86_MAC, X86_WINDOWS
+			//AMD64_LINUX, x86_64_LINUX, X86_FREEBSD, 
+			X86_LINUX, X86_MAC, X86_WINDOWS
 	};
 
 	public static Platform		platform		= findPlatform();
