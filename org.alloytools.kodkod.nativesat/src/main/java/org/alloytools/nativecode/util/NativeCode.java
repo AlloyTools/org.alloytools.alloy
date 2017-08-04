54e466e288dacd889b846f1c3d3d41e21154fc7d
Peter Kriens <peter.kriens@aqute.biz>
2017-08-04 08:56:37 +0200

    [native] Getting linux native code to work

--- org.alloytools.kodkod.nativesat/src/main/java/org/alloytools/nativecode/util/NativeCode.java
+++ org.alloytools.kodkod.nativesat/src/main/java/org/alloytools/nativecode/util/NativeCode.java
@@ -1,6 +1,5 @@
 package org.alloytools.nativecode.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -30,14 +29,15 @@ public class NativeCode {
 	}
 	static Map<String,Path> cached = new ConcurrentHashMap<>();
 
-//	public static Platform		AMD64_LINUX		= new Platform("linux", "amd64", "amd64-linux");
+	public static Platform		AMD64_LINUX		= new Platform("linux", "amd64", "amd64-linux");
 //	public static Platform		x86_64_LINUX	= new Platform("linux", "x86-64", "linux_x86_64");
 //	public static Platform		X86_FREEBSD		= new Platform("freebsd", "x86.*", "x86-freebsd");
 	public static Platform		X86_LINUX		= new Platform("linux", ".*86.*", "x86-linux");
 	public static Platform		X86_MAC			= new Platform("mac\\s*os.*", "ppc|power|powerpc.*|x86.*", "x86-mac");
 	public static Platform		X86_WINDOWS		= new Platform("win.*", "x86.*", "x86-windows");
 	public static Platform[]	platforms		= {
-			//AMD64_LINUX, x86_64_LINUX, X86_FREEBSD, 
+			AMD64_LINUX, 
+			// x86_64_LINUX, X86_FREEBSD, 
 			X86_LINUX, X86_MAC, X86_WINDOWS
 	};
 
@@ -53,10 +53,13 @@ public class NativeCode {
 			String libraryName = System.mapLibraryName(name);
 
 			Enumeration<URL> enumeration = NativeCode.class.getClassLoader().getResources(platform.dir+"/"+libraryName);
-			if ( !enumeration.hasMoreElements() )
+			if ( !enumeration.hasMoreElements() ) {
+				System.out.println( "Could not find native lib");
 				return false;
+			}
 
 			URL resource = enumeration.nextElement();
+			System.out.println( "Found native lib '" + resource + "'");
 
 
 			Path to = cached.computeIfAbsent(name, (k)-> {
@@ -87,8 +90,10 @@ public class NativeCode {
 		String arch = System.getProperty("os.arch");
 		System.out.println( "OS _ ARCH = '" + os + "' - '" + arch + "'");
 		for (Platform p : platforms) {
-			if (p.osarch.matcher(arch).matches() && p.osname.matcher(os).matches())
+			if (p.osarch.matcher(arch).matches() && p.osname.matcher(os).matches()) {
+				System.out.println( "Found = '" + platform.dir);
 				return platform = p;
+			}
 		}
 		return new Platform(".*", ".*", null);
 	}
