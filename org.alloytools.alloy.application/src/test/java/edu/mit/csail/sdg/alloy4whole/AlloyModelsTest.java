7abd6cb6fc3e6be22f4fcad5a32ded41220aa242
Peter Kriens <peter.kriens@aqute.biz>
2017-11-17 11:26:35 +0100

    [recurse] When recursion is used a number of pre-checks (ints/strings) fail.

---
+++ org.alloytools.alloy.application/src/test/java/edu/mit/csail/sdg/alloy4whole/AlloyModelsTest.java
@@ -0,0 +1,31 @@
+package edu.mit.csail.sdg.alloy4whole;
+
+import org.junit.Test;
+
+import edu.mit.csail.sdg.alloy4.A4Reporter;
+import edu.mit.csail.sdg.ast.Command;
+import edu.mit.csail.sdg.ast.Module;
+import edu.mit.csail.sdg.parser.CompUtil;
+import edu.mit.csail.sdg.translator.A4Options;
+import edu.mit.csail.sdg.translator.A4Solution;
+import edu.mit.csail.sdg.translator.TranslateAlloyToKodkod;
+
+public class AlloyModelsTest {
+
+	@Test
+	public void testRecursion() throws Exception {
+		String filename = "src/test/resources/test-recursion.als";
+		Module world = CompUtil.parseEverything_fromFile(A4Reporter.NOP, null, filename);
+
+		A4Options options = new A4Options();
+		for (Command command : world.getAllCommands()) {
+			A4Solution ans = TranslateAlloyToKodkod.execute_command(A4Reporter.NOP, world.getAllReachableSigs(),
+					command, options);
+			while (ans.satisfiable()) {
+				String hc = "Answer: " + ans.toString().hashCode();
+				ans = ans.next();
+			}
+			return;
+		}
+	}
+}
