b119f6aa07baab06c1013d1df12d47ac3fd5d2ca
PaulMeng <baolmeng@gmail.com>
2018-06-12 16:23:52 -0500

    Format

--- src/main/java/edu/uiowa/alloy2smt/Main.java
+++ src/main/java/edu/uiowa/alloy2smt/Main.java
@@ -11,39 +11,37 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Alloy to SMT");
-
-        Options options = new Options();
-
+        Options             options             = new Options();
+        CommandLineParser   commandLineParser   = new DefaultParser();        
+        
         options.addOption(Option.builder("i").longOpt("input").hasArg().required().build());
         options.addOption(Option.builder("o").longOpt("output").hasArg().build());
-
-        CommandLineParser commandLineParser = new DefaultParser();
+        
         try
         {
             CommandLine command = commandLineParser.parse(options, args);
+            
             if (command.hasOption("i"))
             {
-                String inputFile  = command.getOptionValue("i");
-                File file = new File(inputFile);
+                String  inputFile   = command.getOptionValue("i");
+                File    file        = new File(inputFile);
 
                 if((file.exists() && file.canRead()))
                 {
-                    CompModule alloyModel = CompUtil.parseEverything_fromFile(null, null,
-                            file.getAbsolutePath());
-
-                    Alloy2SMTTranslator translator = new Alloy2SMTTranslator();
-                    SMTAst root = translator.execute();
-                    SMTLibPrettyPrinter printer = new SMTLibPrettyPrinter(root);
-                    String output = printer.print();
+                    CompModule              alloyModel  = CompUtil.parseEverything_fromFile(null, null, file.getAbsolutePath());
+                    Alloy2SMTTranslator     translator  = new Alloy2SMTTranslator();
+                    SMTAst                  root        = translator.execute();
+                    SMTLibPrettyPrinter     printer     = new SMTLibPrettyPrinter(root);
+                    String                  output      = printer.print();
 
                     if(command.hasOption("o"))
                     {
                         //ToDo: validation
                         File outputFile = new File(command.getOptionValue("o"));
-                        Formatter formatter = new Formatter(outputFile);
-                        formatter.format("%s", output);
-                        formatter.close();
+                        
+                        try (Formatter formatter = new Formatter(outputFile)) {
+                            formatter.format("%s", output);
+                        }
                     }
                     else
                     {
