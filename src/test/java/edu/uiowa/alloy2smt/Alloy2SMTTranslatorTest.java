998798678b0774d6de9bf7cb1e92c8986423472b
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-06-28 05:01:20 -0500

    No multiplicity

--- src/test/java/edu/uiowa/alloy2smt/Alloy2SMTTranslatorTest.java
+++ src/test/java/edu/uiowa/alloy2smt/Alloy2SMTTranslatorTest.java
@@ -16,6 +16,37 @@ class Alloy2SMTTranslatorTest
     }
 
     @Test
+    public void unaryFieldRelation()
+    {
+
+        String input =
+                "sig Addr {}\n" +
+                        "sig Book {\n" +
+                        "addr: Addr}";
+
+        String actual = Utils.translateFromString(input);
+        String expected =
+                "(set-logic ALL)\n" +
+                        "(set-option :produce-models true)\n" +
+                        "(set-option :finite-model-find true)\n" +
+                        "(declare-sort Atom 0)\n" +
+                        "(declare-fun this_Addr () (Set (Tuple Atom )))\n" +
+                        "(declare-fun this_Book () (Set (Tuple Atom )))\n" +
+                        "(declare-fun this_Book_addr () (Set (Tuple Atom Atom )))\n" +
+                        "(assert (subset this_Book_addr (product this_Book this_Addr)))\n" +
+                        "(assert (forall ((_x1 Atom)) (=> (member (mkTuple _x1 ) this_Book) " +
+                        "(exists ((_x2 Atom)) " +
+                        "(and (member (mkTuple _x2 ) this_Addr) " +
+                        "(and (member (mkTuple _x1 _x2 ) this_Book_addr) " +
+                        "(forall ((_x3 Atom)) (=> (and " +
+                        "(member (mkTuple _x3 ) this_Addr) " +
+                        "(not (= _x3 _x2))) " +
+                        "(not (member (mkTuple _x1 _x3 ) this_Book_addr))))))))))\n";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void unaryFieldRelationOne()
     {
 
@@ -118,7 +149,6 @@ class Alloy2SMTTranslatorTest
 
         assertEquals(expected, actual);
     }
-
 
     @Test
     public void unaryFieldRelationSetSet()
