94a25d6ed5e7a0f74ce2e8c8374be0972caca040
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-12-18 09:46:47 -0600

    Enabled executing cvc4

--- org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/Cvc4SimpleTask.java
+++ org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/Cvc4SimpleTask.java
@@ -9,6 +9,9 @@ import java.util.concurrent.TimeUnit;
 
 public class Cvc4SimpleTask implements WorkerEngine.WorkerTask
 {
+    public static final String OS                  = System.getProperty("os.name");
+    public static final String SEP                 = File.separator;
+    public static final String BIN_PATH = System.getProperty("user.dir")+SEP+"bin"+SEP;
     public static final int TIMEOUT = 300;
 
     private final Map<String, String> alloyFiles;
@@ -20,62 +23,52 @@ public class Cvc4SimpleTask implements WorkerEngine.WorkerTask
     @Override
     public void run(WorkerEngine.WorkerCallback workerCallback) throws Exception
     {
-        // translate alloy file to smt2 file
-        try
+        //ToDo: implement the case when there are multiple files
+        Iterator<Map.Entry<String, String>> iterator = alloyFiles.entrySet().iterator();
+
+        Map.Entry<String, String> entry = iterator.next();
+        String fileName                 = entry.getKey();
+        String smtFileName              = fileName + ".smt2";
+
+        ProcessBuilder  processBuilder  = new ProcessBuilder();
+        String [] command               = new String[]{
+                "java",
+                "-jar",
+                "bin" + SEP + "alloy2smt.jar",
+                "-o",
+                smtFileName
+        };
+
+        processBuilder.command(command);
+
+        workerCallback.callback("Executing command: " + String.join(" ", command));
+
+
+        Process process           = processBuilder.start();
+
+        OutputStream processInput = process.getOutputStream();
+
+        processInput.write(entry.getValue().getBytes());
+        processInput.close();
+
+        if(process.waitFor((long) TIMEOUT, TimeUnit.SECONDS))
         {
-            //ToDo: implement the case when there are multiple files
-            Iterator<Map.Entry<String, String>> iterator = alloyFiles.entrySet().iterator();
+            String error = getProcessOutput(process.getErrorStream());
 
-            Map.Entry<String, String> entry = iterator.next();
-            String fileName                 = entry.getKey();
-
-            ProcessBuilder  processBuilder  = new ProcessBuilder();
-            String [] command        = new String[]{
-                    "java",
-                    "-jar",
-                    "alloy2smt.jar",
-//                    "-i",
-//                    fileName, // input file
-                    "-o",
-                    fileName + ".smt2"
-            };
-
-            processBuilder.command(command);
-
-            workerCallback.callback("Executing command: " + String.join(" ", command));
-
-
-            Process process = processBuilder.start();
-
-            OutputStream processInput = process.getOutputStream();
-
-            processInput.write(entry.getValue().getBytes());
-            processInput.close();
-
-            if(process.waitFor((long) TIMEOUT, TimeUnit.SECONDS))
+            if(!error.isEmpty())
             {
-                String error = getProcessOutput(process.getErrorStream());
-
-                if(!error.isEmpty())
-                {
-                    throw new Exception(error);
-                }
-
-                workerCallback.callback("Finished translating from alloy to smt2: " + fileName + ".smt2");
-
-                String output = getProcessOutput(process.getInputStream());
-
-                workerCallback.callback("Translation output:" + output);
+                throw new Exception(error);
             }
-            else
-            {
-                workerCallback.callback("Timeout: Translation from ally to smt2 did not finish after " + TIMEOUT  + " seconds");
-            }
+
+            String output = getProcessOutput(process.getInputStream());
+
+            workerCallback.callback("Translation output:" + output + "\n");
+
+            executeCVC4(smtFileName, workerCallback);
         }
-        catch (Exception exception)
+        else
         {
-            exception.printStackTrace();
-            throw exception;
+            workerCallback.callback("Timeout: Translation from ally to smt2 did not finish after " + TIMEOUT  + " seconds");
         }
     }
 
@@ -91,5 +84,72 @@ public class Cvc4SimpleTask implements WorkerEngine.WorkerTask
         }
 
         return stringBuilder.toString();
+    }
+
+    private static boolean isNumeric(String str)
+    {
+        return str.matches("\\d+(\\.\\d+)?");
+    }
+    private void executeCVC4(String fileName, WorkerEngine.WorkerCallback workerCallback) throws Exception
+    {
+        String cvc4;
+
+        if(OS.startsWith("Windows"))
+        {
+            cvc4 = BIN_PATH + "cvc4_win64.exe";
+        }
+        else if(OS.startsWith("Linux"))
+        {
+            cvc4 = BIN_PATH + "cvc4_linux";
+        }
+        else
+        {
+            throw new Exception("No CVC4 binary availble for the OS: " + OS);
+        }
+        if(fileName == null)
+        {
+            throw new Exception("No input file for CVC4!");
+        }
+
+        File cvc4Binary = new File(cvc4);
+
+        if(!cvc4Binary.exists())
+        {
+            throw new Exception("CVC4 binary does not exist at: " + cvc4);
+        }
+        if(!cvc4Binary.canExecute())
+        {
+            throw new Exception("CVC4 binary cannot be executed at: " + cvc4);
+        }
+
+        ProcessBuilder  processBuilder = new ProcessBuilder();
+        List<String>    command       = new ArrayList<>();
+
+        command.add(cvc4);
+        command.add(fileName);
+
+        processBuilder.command(command);
+
+        workerCallback.callback("Executing command: " + String.join(" ", command));
+
+        Process process = processBuilder.start();
+
+        if(process.waitFor(TIMEOUT, TimeUnit.SECONDS))
+        {
+            String error = getProcessOutput(process.getErrorStream());
+
+            if(!error.isEmpty())
+            {
+                throw new Exception(error);
+            }
+
+            String cvc4Output = getProcessOutput(process.getInputStream());
+            workerCallback.callback("\nCVC4 Output:\n" + cvc4Output);
+        }
+        else
+        {
+            workerCallback.callback("CVC4 Timeout: " + TIMEOUT + " seconds");
+        }
+        process.destroy();
     }
 }
