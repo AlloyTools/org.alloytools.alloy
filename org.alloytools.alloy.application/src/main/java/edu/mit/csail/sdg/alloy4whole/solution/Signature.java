43e0e7d321e511f7f9243cfe382fd5eb4ba4b876
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-12-24 16:23:40 -0600

    More members for class Signatur

--- org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/solution/Signature.java
+++ org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/solution/Signature.java
@@ -21,5 +21,29 @@ public class Signature
     public int parentId;
 
     @XmlAttribute(name = "builtin")
-    public String builtIn;
+    public String builtIn; // yes/no
+
+    @XmlAttribute(name = "abstract")
+    public String isAbstract; // yes/no
+
+    @XmlAttribute(name = "one")
+    public String isOne; // yes/no
+
+    @XmlAttribute(name = "lone")
+    public String isLone; // yes/no
+
+    @XmlAttribute(name = "some")
+    public String isSome; // yes/no
+
+    @XmlAttribute(name = "private")
+    public String isPrivate; // yes/no
+
+    @XmlAttribute(name = "meta")
+    public String isMeta; // yes/no
+
+    @XmlAttribute(name = "exact")
+    public String isExact; // yes/no
+
+    @XmlAttribute(name = "enum")
+    public String isEnum; // yes/no
 }
