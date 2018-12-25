9369a1cc2d308bf84048e44bd3e18104bbfe1af5
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-12-25 07:14:12 -0600

    change

--- src/main/java/edu/uiowa/alloy2smt/mapping/MappingSignature.java
+++ src/main/java/edu/uiowa/alloy2smt/mapping/MappingSignature.java
@@ -1,10 +1,16 @@
+/*
+ * This file is part of alloy2smt.
+ * Copyright (C) 2018-2019  The University of Iowa
+ *
+ * @author Mudathir Mohamed, Paul Meng
+ *
+ */
+
 package edu.uiowa.alloy2smt.mapping;
 
-
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.List;
+
 
 @XmlRootElement(name = "Signature")
 public class MappingSignature
