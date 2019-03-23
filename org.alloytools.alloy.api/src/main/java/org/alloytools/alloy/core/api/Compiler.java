99f4a0cf3c6e5c92cad4f1903be33fe4966155f1
Aleksandar Milicevic <aleks@csail.mit.edu>
2019-03-23 12:13:00 -0700

    Minor edits in Compiler.java

--- org.alloytools.alloy.api/src/main/java/org/alloytools/alloy/core/api/Compiler.java
+++ org.alloytools.alloy.api/src/main/java/org/alloytools/alloy/core/api/Compiler.java
@@ -3,7 +3,8 @@ package org.alloytools.alloy.core.api;
 import java.io.File;
 
 /**
- * An Alloy Compiler that can compile a file or a source and provide an {@link Module}
+ * An Alloy Compiler that can compile a file or a source and provide a
+ * {@link Module}
  */
 public interface Compiler {
 	/**
@@ -14,22 +15,20 @@ public interface Compiler {
 	Module compileSource(String source);
 
 	/**
-	 * Compile a path, the path is resolved via the resolver. Any opens in the
-	 * content will be also recursively compiled.
+	 * Compile a path, the path is resolved via the resolver. Any explicit
+	 * imports in the content (specified using the 'open' keyword) will also be
+	 * recursively compiled.
 	 * 
 	 * @return a Module
 	 */
 	Module compile(String path);
 
 	/**
-	 * Compile a file. Any opens in the content will be also recursively
-	 * compiled. 
+	 * Compile a file. Any explicit imports in the content (specified using the
+	 * 'open' keyword) will also be recursively compiled.
 	 * 
-	 * @param file
-	 *            the file
+	 * @param file the file
 	 * @return a Module
 	 */
 	Module compile(File file);
-
-	String resolve(String path);
 }
