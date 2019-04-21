87476f6b9f7f100e7435944eaae1c70e5dccc794
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-04-21 12:27:18 -0500

    refactoring

--- alloy2smt/src/main/java/edu/uiowa/alloy2smt/smt/parser/antlr/antlr.bat
+++ alloy2smt/src/main/java/edu/uiowa/alloy2smt/smt/parser/antlr/antlr.bat
@@ -1,1 +1,1 @@
-java -jar antlr-4.7.2-complete.jar Smt.g4 -visitor -package edu.uiowa.alloy2smt.smtparser.antlr
+java -jar antlr-4.7.2-complete.jar Smt.g4 -visitor -package edu.uiowa.alloy2smt.smt.parser.antlr
