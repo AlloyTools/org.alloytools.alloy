61c60e8583a1c05b7e3841bf3a0e4ca5bebea82f
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-05-11 13:55:24 -0500

    refactoring

--- alloy2smt/src/test/java/edu/uiowa/alloy2smt/translators/SampleModelTests.java
+++ alloy2smt/src/test/java/edu/uiowa/alloy2smt/translators/SampleModelTests.java
@@ -1,83 +1,69 @@
 package edu.uiowa.alloy2smt.translators;
 
 import edu.uiowa.alloy2smt.Utils;
+import edu.uiowa.alloy2smt.utils.AlloyUtils;
+import edu.uiowa.alloy2smt.utils.CommandResult;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class SampleModelTests
 {
-    private Translation getExample(String fileName) throws IOException
+    @Test
+    public void farmer() throws Exception
     {
-        String content = new String(Files.readAllBytes(Paths.get(fileName)));
-        Translation translation = Utils.translate(content);
-        return translation;
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/tutorial/farmer.als");
     }
 
     @Test
-    public void farmer() throws IOException
+    public void abstractMemory() throws Exception
     {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/examples/tutorial/farmer.als");
-
-        String script = translation.translateAllCommandsWithCheckSat();
-
-        Assertions.assertTrue(script.contains("(declare-fun ord_next"));
-        Assertions.assertTrue(script.contains("(declare-fun ord_last"));
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/book/chapter6/memory/abstractMemory.als");
     }
 
     @Test
-    public void abstractMemory() throws IOException
+    public void cacheMemory() throws Exception
     {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/book/chapter6/memory/abstractMemory.als");
-
-        String writeIdempotent =  translation.translateCommand(1);
-
-        Assertions.assertFalse(writeIdempotent.contains("m\""));
-    }
-
-    @Test
-    public void cacheMemory() throws IOException
-    {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/book/chapter6/memory/cacheMemory.als");
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/book/chapter6/memory/cacheMemory.als");
         // ToDo: fix the exception thrown by the next expression: by supporting setof operator
         //  "set c . (this/CacheSystem <: main) . this/Data - c . (this/CacheSystem <: cache) . this/Data"
     }
 
     @Test
-    public void hotel1() throws IOException
+    public void hotel1() throws Exception
     {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/book/chapter6/hotel1.als");
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/book/chapter6/hotel1.als");
         // ToDo: support multiplicity constraints on relations with arity GT 3!
     }
 
     @Test
-    public void messaging() throws IOException
+    public void messaging() throws Exception
     {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/examples/algorithms/messaging.als");
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/algorithms/messaging.als");
         // ToDo: support ONE_ARROW_ONE
     }
 
     @Test
-    public void ringlead() throws IOException
+    public void ringlead() throws Exception
     {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/examples/algorithms/ringlead.als");
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/algorithms/ringlead.als");
         // ToDo: support # needsToSend = # { m: reads | m.state.id in nodeOrd/nexts[self] }
     }
 
     @Test
-    public void stable_mutex_ring() throws IOException
+    public void stable_mutex_ring() throws Exception
     {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/examples/algorithms/stable_mutex_ring.als");
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/algorithms/stable_mutex_ring.als");
         // ToDo: support ONE_ARROW_ONE
     }
 
     @Test
-    public void firewire() throws IOException
+    public void firewire() throws Exception
     {
-        Translation translation = getExample("../org.alloytools.alloy.extra/extra/models/examples/case_studies/firewire.als");
+        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/case_studies/firewire.als");
         // ToDo: support LONE_ARROW_ANY
     }
 }
