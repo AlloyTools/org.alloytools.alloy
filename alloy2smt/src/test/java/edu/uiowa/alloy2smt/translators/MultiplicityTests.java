62fb10fdb6d3531f78e646f3c6cc55d6801d7297
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-06-05 00:44:26 -0500

    A one -> lone A A lone -> lone A

--- alloy2smt/src/test/java/edu/uiowa/alloy2smt/translators/MultiplicityTests.java
+++ alloy2smt/src/test/java/edu/uiowa/alloy2smt/translators/MultiplicityTests.java
@@ -27,6 +27,22 @@ class MultiplicityTests
     }
 
     @Test
+    public void oneLoneSetDeclaration() throws Exception
+    {
+        String alloy =  "sig A {} fact f {#A = 1 and some x: A one -> lone A | some A}";
+        List<CommandResult> results =  AlloyUtils.runAlloyString(alloy, false);
+        assertEquals ("sat", results.get(0).satResult);
+    }
+
+    @Test
+    public void loneLoneSetDeclaration() throws Exception
+    {
+        String alloy =  "sig A {} fact f {#A = 1 and some x: A lone -> lone A | some A}";
+        List<CommandResult> results =  AlloyUtils.runAlloyString(alloy, false);
+        assertEquals ("sat", results.get(0).satResult);
+    }
+
+    @Test
     public void loneSetDeclaration() throws Exception
     {
         String alloy =  "sig A {} fact f {#A = 1 and some x: lone A | lone x and no x}";
