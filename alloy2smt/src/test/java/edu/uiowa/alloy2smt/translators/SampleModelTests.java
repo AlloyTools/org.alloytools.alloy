2964978b3ba9740aeb91b6db82cc599c62f31101
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-06-10 12:18:38 -0500

    functions dependencies across modules

--- alloy2smt/src/test/java/edu/uiowa/alloy2smt/translators/SampleModelTests.java
+++ alloy2smt/src/test/java/edu/uiowa/alloy2smt/translators/SampleModelTests.java
@@ -10,60 +10,80 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SampleModelTests
 {
     @Test
     public void farmer() throws Exception
     {
         List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/tutorial/farmer.als", true);
+        assertEquals("sat", results.get(0).satResult);
+        assertEquals("unsat", results.get(1).satResult);
     }
 
     @Test
     public void abstractMemory() throws Exception
     {
         List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/book/chapter6/memory/abstractMemory.als", true);
+
+        assertEquals("unsat", results.get(0).satResult);
+        assertEquals("unsat", results.get(1).satResult);
     }
 
     @Test
     public void cacheMemory() throws Exception
     {
         List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/book/chapter6/memory/cacheMemory.als", true);
+
+        assertEquals("unsat", results.get(0).satResult);
         // ToDo: fix the exception thrown by the next expression: by supporting setof operator
         //  "set c . (this/CacheSystem <: main) . this/Data - c . (this/CacheSystem <: cache) . this/Data"
     }
 
-    @Test
-    public void hotel1() throws Exception
-    {
-        List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/book/chapter6/hotel1.als", true);
-        // ToDo: support multiplicity constraints on relations with arity GT 3!
-    }
 
     @Test
     public void messaging() throws Exception
     {
         List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/algorithms/messaging.als", true);
-        // ToDo: support ONE_ARROW_ONE
+
+        assertEquals("sat", results.get(0).satResult);
+        assertEquals("sat", results.get(1).satResult);
     }
 
     @Test
     public void ringlead() throws Exception
     {
         List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/algorithms/ringlead.als", true);
-        // ToDo: support # needsToSend = # { m: reads | m.state.id in nodeOrd/nexts[self] }
+
+        assertEquals("sat", results.get(0).satResult);
+        assertEquals("unsat", results.get(1).satResult);
+        assertEquals("unsat", results.get(2).satResult);
+        assertEquals("sat", results.get(3).satResult);
+        assertEquals("unsat", results.get(4).satResult);
     }
 
     @Test
     public void stable_mutex_ring() throws Exception
     {
         List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/algorithms/stable_mutex_ring.als", true);
-        // ToDo: support ONE_ARROW_ONE
+
+        assertEquals("sat", results.get(0).satResult);
+        assertEquals("sat", results.get(1).satResult);
+        assertEquals("unsat", results.get(2).satResult);
     }
 
     @Test
     public void firewire() throws Exception
     {
         List<CommandResult> results = AlloyUtils.runAlloyFile("../org.alloytools.alloy.extra/extra/models/examples/case_studies/firewire.als", true);
-        // ToDo: support LONE_ARROW_ANY
+
+        assertEquals("sat", results.get(0).satResult);
+        assertEquals("sat", results.get(1).satResult);
+        assertEquals("unsat", results.get(2).satResult);
+        assertEquals("unsat", results.get(3).satResult);
+        assertEquals("unsat", results.get(4).satResult);
+        assertEquals("unsat", results.get(5).satResult);
+        assertEquals("sat", results.get(6).satResult);
     }
 }
