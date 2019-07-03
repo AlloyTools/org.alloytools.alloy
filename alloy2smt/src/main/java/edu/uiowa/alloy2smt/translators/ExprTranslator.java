09105677cfc1ea2f65e66641fe61845e4070ebcb
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-07-02 20:13:06 -0500

    refactored ExprCallTranslator

--- alloy2smt/src/main/java/edu/uiowa/alloy2smt/translators/ExprTranslator.java
+++ alloy2smt/src/main/java/edu/uiowa/alloy2smt/translators/ExprTranslator.java
@@ -29,12 +29,15 @@ public class ExprTranslator
 
     final ExprQtTranslator exprQtTranslator;
 
+    final ExprCallTranslator exprCallTranslator;
+
     public ExprTranslator(Alloy2SmtTranslator translator)
     {
         this.translator = translator;
         this.exprUnaryTranslator = new ExprUnaryTranslator(this);
         this.exprBinaryTranslator = new ExprBinaryTranslator(this);
         this.exprQtTranslator = new ExprQtTranslator(this);
+        this.exprCallTranslator = new ExprCallTranslator(this);
     }
 
     Expression translateExpr(Expr expr)
@@ -70,7 +73,7 @@ public class ExprTranslator
         }
         else if (expr instanceof ExprCall)
         {
-            return translateExprCall((ExprCall) expr, environment);
+            return exprCallTranslator.translateExprCall((ExprCall) expr, environment);
         }
         else if (expr instanceof ExprITE)
         {
@@ -177,62 +180,6 @@ public class ExprTranslator
         return new LetExpression(varToExprMap, letBodyExpr);
     }
 
-    Expression translateExprCall(ExprCall exprCall, Environment environment)
-    {
-        String funcName = exprCall.fun.label;
-        List<Expression> argExprs = new ArrayList<>();
-
-        for (Expr e : exprCall.args)
-        {
-            argExprs.add(translateExpr(e, environment));
-        }
-
-//        if (this.translator.funcNamesMap.containsKey(funcName))
-//        {
-//            return new FunctionCallExpression(translator.getFunctionFromAlloyName(funcName), argExprs);
-//        }
-//        else if (this.translator.setComprehensionFuncNameToInputsMap.containsKey(funcName))
-//        {
-//            return translateSetComprehensionFuncCallExpr(funcName, argExprs);
-//        }
-        if (funcName.equals("integer/plus") || funcName.equals("integer/add"))
-        {
-            return translateArithmetic(argExprs.get(0), argExprs.get(1), BinaryExpression.Op.PLUS, environment);
-        }
-        else if (funcName.equals("integer/minus") || funcName.equals("integer/sub"))
-        {
-            return translateArithmetic(argExprs.get(0), argExprs.get(1), BinaryExpression.Op.MINUS, environment);
-        }
-        else if (funcName.equals("integer/mul"))
-        {
-            return translateArithmetic(argExprs.get(0), argExprs.get(1), BinaryExpression.Op.MULTIPLY, environment);
-        }
-        else if (funcName.equals("integer/div"))
-        {
-            return translateArithmetic(argExprs.get(0), argExprs.get(1), BinaryExpression.Op.DIVIDE, environment);
-        }
-        else if (funcName.equals("integer/rem"))
-        {
-            return translateArithmetic(argExprs.get(0), argExprs.get(1), BinaryExpression.Op.MOD, environment);
-        }
-//        else if (translator.functionsMap.containsKey(funcName))
-//        {
-//            FunctionDeclaration function = translator.getFunction(funcName);
-//            return new FunctionCallExpression(function, argExprs);
-//        }
-        else
-        {
-            Expr body = exprCall.fun.getBody();
-
-            for (int i = 0; i < exprCall.args.size(); i++)
-            {
-                body = AlloyUtils.substituteExpr(body, exprCall.fun.get(i), exprCall.args.get(i));
-            }
-            Expression callExpression = translateExpr(body);
-            return callExpression;
-        }
-    }
-
     public Expression translateSetComprehensionFuncCallExpr(String funcName, List<Expression> argExprs)
     {
         Map<String, Expression> letVars = new HashMap<>();
@@ -261,147 +208,6 @@ public class ExprTranslator
         return setBdVar.getVariable();
     }
 
-    public Expression translateArithmetic(Expression A, Expression B, BinaryExpression.Op op, Environment environment)
-    {
-
-        A = convertIntConstantToSet(A);
-
-        B = convertIntConstantToSet(B);
-
-        // for all x, y : uninterpretedInt. x in A and y in B implies
-        // exists z :uninterpretedInt. (x, y, z) in operation
-
-
-        if (A.getSort().equals(AbstractTranslator.setOfIntSortTuple))
-        {
-            A = translator.handleIntConstant(A);
-        }
-
-        if (B.getSort().equals(AbstractTranslator.setOfIntSortTuple))
-        {
-            B = translator.handleIntConstant(B);
-        }
-
-        Expression newA = A;
-        Expression newB = B;
-
-        // add variables in the environment as arguments to the result function
-        LinkedHashMap<String, Expression> argumentsMap = environment.getVariables();
-        List<Sort> argumentSorts = new ArrayList<>();
-        List<Expression> arguments = new ArrayList<>();
-        List<VariableDeclaration> quantifiedArguments = new ArrayList<>();
-        for (Map.Entry<String, Expression> argument : argumentsMap.entrySet())
-        {
-            arguments.add(argument.getValue());
-            Variable variable = (Variable) argument.getValue();
-            Sort sort = variable.getSort();
-            argumentSorts.add(sort);
-
-            // handle set sorts differently to avoid second order quantification
-            if (sort instanceof SetSort)
-            {
-                Sort elementSort = ((SetSort) sort).elementSort;
-                VariableDeclaration tuple = new VariableDeclaration(variable.getName(), elementSort, null);
-                quantifiedArguments.add(tuple);
-                Expression singleton = UnaryExpression.Op.SINGLETON.make(tuple.getVariable());
-                newA = newA.replace(argument.getValue(), singleton);
-                newB = newB.replace(argument.getValue(), singleton);
-                Expression constraint = ((VariableDeclaration) variable.getDeclaration()).getConstraint();
-                if (constraint != null)
-                {
-                    constraint = constraint.replace(variable, singleton);
-                }
-                tuple.setConstraint(constraint);
-            }
-            else if (sort instanceof TupleSort || sort instanceof UninterpretedSort)
-            {
-                quantifiedArguments.add((VariableDeclaration) variable.getDeclaration());
-            }
-            else
-            {
-                throw new UnsupportedOperationException();
-            }
-        }
-
-        FunctionDeclaration result = new FunctionDeclaration(TranslatorUtils.getNewSetName(), argumentSorts, AbstractTranslator.setOfUninterpretedIntTuple);
-        translator.smtProgram.addFunction(result);
-
-        Expression resultExpression;
-        if (result.getInputSorts().size() > 0)
-        {
-            resultExpression = new FunctionCallExpression(result, arguments);
-        }
-        else
-        {
-            resultExpression = result.getVariable();
-        }
-
-        VariableDeclaration x = new VariableDeclaration("__x__", AbstractTranslator.uninterpretedInt, null);
-        VariableDeclaration y = new VariableDeclaration("__y__", AbstractTranslator.uninterpretedInt, null);
-        VariableDeclaration z = new VariableDeclaration("__z__", AbstractTranslator.uninterpretedInt, null);
-
-        Expression xTuple = new MultiArityExpression(MultiArityExpression.Op.MKTUPLE, x.getVariable());
-        Expression yTuple = new MultiArityExpression(MultiArityExpression.Op.MKTUPLE, y.getVariable());
-        Expression zTuple = new MultiArityExpression(MultiArityExpression.Op.MKTUPLE, z.getVariable());
-
-        Expression xValue = new FunctionCallExpression(AbstractTranslator.uninterpretedIntValue, x.getVariable());
-        Expression yValue = new FunctionCallExpression(AbstractTranslator.uninterpretedIntValue, y.getVariable());
-        Expression zValue = new FunctionCallExpression(AbstractTranslator.uninterpretedIntValue, z.getVariable());
-
-        Expression xMember = BinaryExpression.Op.MEMBER.make(xTuple, A);
-        Expression yMember = BinaryExpression.Op.MEMBER.make(yTuple, B);
-        Expression zMember = BinaryExpression.Op.MEMBER.make(zTuple, resultExpression);
-
-        Expression xyOperation = op.make(xValue, yValue);
-        Expression equal = BinaryExpression.Op.EQ.make(xyOperation, zValue);
-
-        Expression and1 = BinaryExpression.Op.AND.make(xMember, yMember);
-        Expression and2 = BinaryExpression.Op.AND.make(equal, and1);
-        Expression exists1 = QuantifiedExpression.Op.EXISTS.make(and2, x, y);
-        Expression argumentConstraints = BoolConstant.True;
-        for (VariableDeclaration declaration : quantifiedArguments)
-        {
-            if (declaration.getConstraint() != null)
-            {
-                argumentConstraints = BinaryExpression.Op.AND.make(argumentConstraints, declaration.getConstraint());
-            }
-        }
-        Expression antecedent1 = BinaryExpression.Op.AND.make(argumentConstraints, zMember);
-        Expression implies1 = BinaryExpression.Op.IMPLIES.make(antecedent1, exists1);
-        List<VariableDeclaration> quantifiers1 = new ArrayList<>(quantifiedArguments);
-        quantifiers1.add(z);
-        Expression forall1 = QuantifiedExpression.Op.FORALL.make(implies1, quantifiers1);
-
-        Assertion assertion1 = new Assertion(String.format("%1$s %2$s %3$s axiom1", op, A, B), forall1);
-        translator.smtProgram.addAssertion(assertion1);
-
-        Expression and3 = BinaryExpression.Op.AND.make(equal, zMember);
-        Expression exists2 = QuantifiedExpression.Op.EXISTS.make(and3, z);
-
-        Expression antecedent2 = BinaryExpression.Op.AND.make(argumentConstraints, and1);
-
-        Expression implies2 = BinaryExpression.Op.IMPLIES.make(antecedent2, exists2);
-        List<VariableDeclaration> quantifiers2 = new ArrayList<>(quantifiedArguments);
-        quantifiers2.add(x);
-        quantifiers2.add(y);
-        Expression forall2 = QuantifiedExpression.Op.FORALL.make(implies2, quantifiers2);
-
-        Assertion assertion2 = new Assertion(String.format("%1$s %2$s %3$s axiom2", op, A, B), forall2);
-        translator.smtProgram.addAssertion(assertion2);
-
-        return resultExpression;
-    }
-
-    private Expression convertIntConstantToSet(Expression A)
-    {
-        if (A instanceof IntConstant)
-        {
-            ConstantDeclaration uninterpretedInt = translator.getUninterpretedIntConstant((IntConstant) A);
-            Expression tuple = new MultiArityExpression(MultiArityExpression.Op.MKTUPLE, uninterpretedInt.getVariable());
-            A = UnaryExpression.Op.SINGLETON.make(tuple);
-        }
-        return A;
-    }
 
     public void declArithmeticOp(BinaryExpression.Op op)
     {
