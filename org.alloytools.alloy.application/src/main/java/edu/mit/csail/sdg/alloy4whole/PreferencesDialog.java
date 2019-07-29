2ea670e382243e97ebd3303941b6b6bd63a4fc4a
nmacedo <nfmmacedo@di.uminho.pt>
2019-07-29 17:48:37 +0100

    initial merge of unbounded, solvers, internals

--- org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/PreferencesDialog.java
+++ org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/PreferencesDialog.java
@@ -32,10 +32,15 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.swing.AbstractAction;
 import javax.swing.AbstractListModel;
@@ -75,7 +80,7 @@ import edu.mit.csail.sdg.alloy4.Subprocess;
 import edu.mit.csail.sdg.translator.A4Options.SatSolver;
 
 /**
- * @modified: Nuno Macedo // [HASLab] electrum-base
+ * @modified: Nuno Macedo // [HASLab] electrum-base, electrum-unbounded
  */
 @SuppressWarnings({
                    "serial"
@@ -281,6 +286,15 @@ public class PreferencesDialog extends JFrame {
             satChoices.remove(SatSolver.GlucoseJNI);
         if (!loadLibrary("cryptominisat"))
             satChoices.remove(SatSolver.CryptoMiniSatJNI);
+        // [HASLab] load unbounded model checking backend
+        if (!staticLibrary("electrod")) {
+            satChoices.remove(SatSolver.ElectrodX);
+            satChoices.remove(SatSolver.ElectrodS);
+        }
+        if (!staticLibrary("NuSMV"))
+            satChoices.remove(SatSolver.ElectrodS);
+        if (!staticLibrary("nuXmv"))
+            satChoices.remove(SatSolver.ElectrodX);
         SatSolver now = Solver.get();
         if (!satChoices.contains(now)) {
             now = SatSolver.LingelingJNI;
@@ -314,6 +328,34 @@ public class PreferencesDialog extends JFrame {
         }
         return output.substring(i).startsWith("s SATISFIABLE");
     }
+
+    // [HASLab]
+    private static boolean staticLibrary(String name) {
+        // check if in java library path
+        final String[] dirs = System.getProperty("java.library.path").split(System.getProperty("path.separator"));
+        for (int i = dirs.length - 1; i >= 0; i--) {
+            final File file = new File(dirs[i] + File.separator + name);
+            if (file.canExecute()) {
+                if ("yes".equals(System.getProperty("debug")))
+                    System.out.println("Loaded: " + name);
+                return true;
+            }
+        }
+        // check if in system path
+        for (String str : (System.getenv("PATH") + ":/usr/local/bin").split(Pattern.quote(File.pathSeparator))) {
+            Path pth = Paths.get(str);
+            if (Files.exists(pth.resolve(name))) {
+                if ("yes".equals(System.getProperty("debug")))
+                    System.out.println("Loaded: " + name);
+                return true;
+            }
+        }
+
+        if ("yes".equals(System.getProperty("debug")))
+            System.out.println("Failed to load: " + name);
+        return false;
+    }
+
 
     private static boolean loadLibrary(String library) {
         boolean loaded = _loadLibrary(library);
