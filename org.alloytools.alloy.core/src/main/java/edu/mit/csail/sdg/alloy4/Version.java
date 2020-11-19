d6615501b52ff8943f8b990dc93104a68e2bc637
nmacedo <nfmmacedo@di.uminho.pt>
2020-11-19 12:14:31 +0000

    code cleanup, documentation

--- org.alloytools.alloy.core/src/main/java/edu/mit/csail/sdg/alloy4/Version.java
+++ org.alloytools.alloy.core/src/main/java/edu/mit/csail/sdg/alloy4/Version.java
@@ -39,10 +39,6 @@ public final class Version {
     public static long    buildnumber  = -1;
     public static Instant builddate    = Instant.ofEpochMilli(0);
     public static String  commit       = "unknown";
-    /**
-     * This is true if this is an experimental version rather than a release
-     * version.
-     */
     public static boolean experimental = true;
     private static String shortversion = "";
 
@@ -73,6 +69,11 @@ public final class Version {
         }
 
     }
+
+    /**
+     * This is true if this is an experimental version rather than a release
+     * version.
+     */
 
     /** Returns the build number. */
     public static long buildNumber() {
