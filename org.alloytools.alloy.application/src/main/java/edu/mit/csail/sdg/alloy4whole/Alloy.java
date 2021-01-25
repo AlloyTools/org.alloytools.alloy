e90972ee82608880928cf2ed2cfc580249239f6d
nmacedo <nfmmacedo@di.uminho.pt>
2021-01-25 10:36:02 +0000

    added simple cli for benchmarks

--- org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/Alloy.java
+++ org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/Alloy.java
@@ -4,6 +4,6 @@ package edu.mit.csail.sdg.alloy4whole;
 public class Alloy {
 
     public static void main(String args[]) throws Exception {
-        SimpleGUI.main(args);
+        SimpleMain.main(args);
     }
 }
