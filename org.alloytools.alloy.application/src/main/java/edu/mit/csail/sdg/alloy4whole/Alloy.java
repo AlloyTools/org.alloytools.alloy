c688f1a474c65cf159d57f25d18ea92a3c7f7376
Tamjid Hossain <tamjidturjo@gmail.com>
2021-04-12 22:16:42 +0600

    Unit Testing for CoreDash to Alloy added

--- org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/Alloy.java
+++ org.alloytools.alloy.application/src/main/java/edu/mit/csail/sdg/alloy4whole/Alloy.java
@@ -5,9 +5,10 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 import edu.mit.csail.sdg.alloy4.A4Reporter;
-import edu.mit.csail.sdg.alloy4.ErrorWarning;
+import edu.mit.csail.sdg.parser.CoreDashToAlloy;
 import edu.mit.csail.sdg.parser.DashModule;
 import edu.mit.csail.sdg.parser.DashOptions;
+import edu.mit.csail.sdg.parser.DashToCoreDash;
 import edu.mit.csail.sdg.parser.DashUtil;
 
 public class Alloy {
@@ -17,28 +18,23 @@ public class Alloy {
 
         System.out.println("Please specify the .dsh file path:");
         Scanner sc = new Scanner(System.in);
-        String filePath = "D:/test.als";//"D:/EHealth.als";//"D:/DashModels/EHealth.dsh";
+        String actual = "D:/DashModels/testdsh.dsh";//"D:/test.als";//"D:/EHealth.als";//"D:/DashModels/EHealth.dsh";
+        String expected = "D:/DashModels/test.dsh";
 
         System.out.println("Please specify the CoreDash output location:");
         String outputDir = "D:/";
 
-        if (!filePath.endsWith(".dsh")) {
+        if (!actual.endsWith(".dsh")) {
             System.err.println("File not supported.\nExpected a Dash file with 'dsh' extension");
             //return;
         }
         sc.close();
 
-        Path path = Paths.get(filePath);
+        Path path = Paths.get(actual);
         Path fileName = path.getFileName();
         DashOptions.outputDir = (outputDir + fileName);
 
-        A4Reporter rep = new A4Reporter() {
-            @Override
-            public void warning(ErrorWarning msg) {
-                System.out.print("Relevance Warning:\n" + (msg.toString().trim()) + "\n\n");
-                System.out.flush();
-            }
-        };
+        A4Reporter rep = new A4Reporter();
 
         boolean parse = true;
 
@@ -50,15 +46,27 @@ public class Alloy {
             System.out.println("=========== Parsing+Typechecking " + fileName + " =============");
             //Module world = CompUtil.parseEverything_fromFile(rep, null, filePath);
 
+            //DashModule dashExpected = DashUtil.parseEverything_fromFileDash(rep, null, expected);
 
-            DashModule dashWorld = DashUtil.parseEverything_fromFileDash(rep, null, filePath);
+            DashModule dashActual = DashUtil.parseEverything_fromFileDash(rep, null, actual);
+            DashToCoreDash.transformToCoreDash(dashActual);
+            CoreDashToAlloy.convertToAlloyAST(dashActual);
+            dashActual = DashModule.resolveAll(rep == null ? A4Reporter.NOP : rep, dashActual);
 
+            //System.out.println("Expected Expr: " + dashExpected.facts.get(0).b.toString());
+            System.out.println("Actual Expr: " + dashActual.facts.get(0).b.toString());
 
-            for(String func: dashWorld.funcs.keySet()) {
-                if (func.contains("pre")) {
-                    System.out.println("Expr: " + dashWorld.funcs.get(func).get(0).body.toString());
-                }
-            }
         }
     }
 }
+
+
+/*
+ * for (String func : dashExpected.funcs.keySet()) { if (func.contains("path"))
+ * { System.out.println("Expected Expr: " +
+ * dashExpected.funcs.get(func).get(0).body + " Type: " +
+ * dashExpected.funcs.get(func).get(0).body.getClass()); } } for (String func :
+ * dashActual.funcs.keySet()) { if (func.contains("path")) {
+ * System.out.println("Actual Expr  : " + dashActual.funcs.get(func).get(0).body
+ * + " Type: " + dashActual.funcs.get(func).get(0).body.getClass()); } }
+ */
