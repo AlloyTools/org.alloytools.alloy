a33362cfacbc9b3b16b820e82352f12dc1d85820
Peter Kriens <Peter.Kriens@aQute.biz>
2024-05-17 17:55:23 +0200

    Preparation for release, moved to Java 17

---
+++ org.alloytools.alloy.dist/conveyor/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("dev.hydraulic.conveyor") version "1.10"
+}
+
+repositories {
+    mavenCentral()
+    google()
+    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    // Use the configurations created by the Conveyor plugin to tell Gradle/Conveyor where to find the artifacts for each platform.
+    linuxAmd64(compose.desktop.linux_x64)
+    macAmd64(compose.desktop.macos_x64)
+    macAarch64(compose.desktop.macos_arm64)
+    windowsAmd64(compose.desktop.windows_x64)
+}
+
+
+// region Work around temporary Compose bugs.
+configurations.all {
+    attributes {
+        // https://github.com/JetBrains/compose-jb/issues/1404#issuecomment-1146894731
+        attribute(Attribute.of("ui", String::class.java), "awt")
+    }
+}
+// endregion
